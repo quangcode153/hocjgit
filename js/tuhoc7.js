745de6abbff8ead3bc483aa4289ec7f675330eec
quangcode153 <quang1532006@gmail.com>
2025-08-14 11:40:06 +0700

    mo dau ve mang cua  js

--- js/tuhoc7.js
+++ js/tuhoc7.js
@@ -1,1 +1,7 @@
 //array
+let arr1=[];
+let n=parseInt(prompt("nhập số lượng phần tử có trong mảng 1"));
+for(let i=0;i<n;i++){
+    arr1[i]=prompt("nhập giá trị của mảng 1:");
+}
+console.log(arr1);
